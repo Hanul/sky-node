6bc8d5bd1ed46252c496124c577f83d6ce4046e9
hanul <hanul@hanul.me>
2021-04-19 08:58:24 +0900

    0.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export { default as SkyNode } from "./SkyNode";
 export { default as DomNode } from "./DomNode";
 export { default as BodyNode } from "./BodyNode";
-export { default as ScrollableDomNode, ScrollableDomNodeOptions, ScrollItemDomNode } from "./ScrollableDomNode";
 export { default as el } from "./el";
+export { default as ScrollableDomNode, ScrollableDomNodeOptions, ScrollItemDomNode } from "./ScrollableDomNode";
 export { default as FloatingDomNode, Position } from "./FloatingDomNode";
 export { default as ClosableFloatingDomNode } from "./ClosableFloatingDomNode";
 export { default as Popup } from "./Popup";
